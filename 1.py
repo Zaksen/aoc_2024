664ae2fb9c78faf3c30bff7550e11b6d28147c17
Zaksen <zakariaderradji@gmail.com>
2024-12-01 09:33:28 +0100

    first day

---
+++ 1.py
@@ -0,0 +1,24 @@
+with open('inputs/1.in') as f:
+    content = f.readlines()
+
+first = []
+second = []
+for l in content:
+    first.append(int(l.strip().split()[0]))
+    second.append(int(l.strip().split()[1]))
+
+first.sort()
+second.sort()
+
+distances = [abs(y - x) for x, y in zip(first, second)]
+p1 = sum(distances)
+print(p1)
+
+p2 = 0
+for i in first:
+    p2 += second.count(i) * i
+
+print(p2)
+
+
+
