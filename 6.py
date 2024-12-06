b0fe0fee87ddba67c95eb59efc71cba44cb5d1b0
Zaksen <37753989+Zaksen@users.noreply.github.com>
2024-12-06 14:34:20 +0100

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,32 +1,34 @@
 with open('inputs/6.in') as f:
     grid = [list(r.strip()) for r in f.readlines()]
 
-covered = set()
 
-directions = {
-    '^' : (0,-1),
-    '>' : (1,0),    
-    'v' : (0,1),    
-    '<' : (-1,0),    
-}
 
 max_x = len(grid) - 1
 max_y = len(grid[0]) - 1
-
 for i in range(max_x):
-    for j in range(max_y):
-        if grid[i][j] == "^":
-            starting_position = (i, j)
-            covered.add(starting_position)
-            break
+        for j in range(max_y):
+            if grid[i][j] == "^":
+                starting_position = (i, j)
+                break
 
-in_grid = True
-
-while in_grid:
-    dir = "^"
+def go(grid, starting_position):
+    covered = set()
+    covered.add(starting_position)
     p = starting_position
-    print(starting_position)
-    x = p[0] + directions[dir][0]
-    y = p[1] + directions[dir][1]
-    if grid[x, y] == '#':
-        dir
+    dir = (-1, 0)
+
+    while True:
+        x = p[0] + dir[0]
+        y = p[1] + dir[1]
+        if x < 0 or y < 0:
+            break
+        if x > max_x or y > max_y:
+            break
+        if grid[x][y] == '#':
+            dir = (dir[1], -dir[0])
+            continue
+        p = (x, y)
+        covered.add(p)
+
+    return len(covered)
+        
